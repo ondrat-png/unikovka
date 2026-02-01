7123da5d29afc96f2cee52e150b773f6cb3880d9
ondrejtomasek <ondrat2009@gmail.com>
2026-02-01 15:38:41 +0100

    command help chancged

--- src/Commands/Help.java
+++ src/Commands/Help.java
@@ -4,15 +4,17 @@ public class Help implements Command{
     @Override
     public String execute(String command) {
         return "\n================ příkazy, které může hráč použít ================\n" +
-                "move (jdi_mistnost)                = jdi místnost\n" +
-                "exit (exit)                        = konec hry\n" +
-                "hint (hint)                        = nápověda hráči\n" +
-                "pickup (pickup_item)               = hráč vezme item\n" +
-                "drop (drop_item)                   = hráč odhodí item\n" +
-                "use (use_item)                     = použití itemu\n" +
-                "search (search_item/search_objejt) = prohledá item/objekt\n" +
-                "inventory (inventory)              = ukáže inventář\n" +
-                "dialoque (dialoque_postava)        = mluveni s postavau";
+                "jdi (místnost)                  = jdi místnost\n" +
+                "konec                           = konec hry\n" +
+                "napoveda                        = nápověda hráči\n" +
+                "vezmi (předmět)                 = hráč vezme item\n" +
+                "zahodit                         = hráč odhodí item\n" +
+                "otevrit (objekt)                = použití itemu\n" +
+                "prohledat                       = prohledá item/objekt\n" +
+                "inventar                        = ukáže inventář\n" +
+                "mluv                            = mluvení s postavou\n" +
+                "pomoc                           = ukáže příkazy\n" +
+                "=====================================================================\n";
 
     }
 
