d10a73c9a72eedf2d397aca12f2b111d4e010332
ondrejtomasek <ondrat2009@gmail.com>
2026-02-02 10:17:18 +0100

    new command mapa

---
+++ src/Commands/Mapa.java
@@ -0,0 +1,34 @@
+package Commands;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+
+public class Mapa implements Command{
+
+    @Override
+    public String execute(String command) {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("res/mapa.txt"));
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
+    }
+}
